854c5e42df26a1898bd3b2f1fa7db8012c8257ba
Cody Frazer <cfrazer@ncsasports.org>
2015-08-02 11:18:48 -0500

    Default JS engine to plain JS

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Inhabit
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Default JS engine to plain .js
+    config.generators.javascript_engine :js
   end
 end
