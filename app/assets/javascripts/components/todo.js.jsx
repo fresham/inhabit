3149274237ecac24db1ca5033a403efecf8b782e
Cody Frazer <cfrazer@ncsasports.org>
2015-08-09 19:02:28 -0500

    Add todo item controls

--- app/assets/javascripts/components/todo.js.jsx
+++ app/assets/javascripts/components/todo.js.jsx
@@ -40,6 +40,10 @@ var TodoItem = React.createClass({
     return (
       <li className="todo-item">
         <span>{this.props.name}</span>
+        <div className="todo-item-controls">
+          <i className="fa fa-pencil" />
+          <i className="fa fa-trash-o" />
+        </div>
       </li>
     );
   }
