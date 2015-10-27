ad377bbd02e62f02e3eb2acac6edc01022957012
Cody Frazer <cfrazer@ncsasports.org>
2015-10-27 16:35:41 -0500

    Add read for todo list data

--- app/api/api.rb
+++ app/api/api.rb
@@ -6,4 +6,9 @@ class API < Grape::API
   get :ping do
     { ping: params[:pong] || "pong" }
   end
+
+  desc "Returns all todo items"
+  get :todos do
+    Todo.all
+  end
 end
--- app/assets/javascripts/components/TodoList.js.jsx
+++ app/assets/javascripts/components/TodoList.js.jsx
@@ -1,11 +1,23 @@
 var TodoList = React.createClass({
+  getInitialState: function () {
+    return { todoItems: [] };
+  },
+
+  componentDidMount: function () {
+    $.get('/api/todos.json', function (data) {
+      if (this.isMounted()) {
+        this.setState({ todoItems: data });
+      }
+    }.bind(this));
+  },
+
   render: function () {
     return (
       <div className="todo-list">
         <ul>
-          <TodoItem name="Todo 1" />
-          <TodoItem name="Todo 2" />
-          <TodoItem name="Todo 3" />
+          {this.state.todoItems.map(function (item) {
+            return <TodoItem name={item.name} />;
+          })}
         </ul>
       </div>
     );
